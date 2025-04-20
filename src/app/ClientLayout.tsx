1c7743246346479473849333829cc16f2cc6f1f4
eugeniucc <160481703+eugeniucc@users.noreply.github.com>
2025-04-20 23:17:10 +0300

    client layout

---
+++ src/app/ClientLayout.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { SpeedInsights } from '@vercel/speed-insights/next'
+import { Analytics } from '@vercel/analytics/react'
+import { JivoChat } from '@/app/components/jivochat'
+import { AgeConfirm } from '@/app/components/AgeConfirm'
+import { HeaderContacts } from '@/app/ui/header/ui/HeaderContacts'
+import { HomeHeader } from '@/app/ui/header/HomeHeader'
+import { HomeFooter } from '@/app/ui/footer/HomeFooter'
+import { I18nextProvider } from 'react-i18next'
+import i18next from './i18n'
+import { TranslatedHead } from './TranslatedHead'
+
+const queryClient = new QueryClient()
+
+export default function ClientLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <TranslatedHead />
+      <I18nextProvider i18n={i18next}>
+        <SpeedInsights />
+        <Analytics />
+        <JivoChat />
+        <AgeConfirm />
+        <HeaderContacts />
+        <HomeHeader />
+        <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+        <HomeFooter />
+      </I18nextProvider>
+    </>
+  )
+}
---
+++ src/app/TranslatedHead.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import { useTranslation } from 'react-i18next'
+
+export const TranslatedHead = () => {
+  const { t, i18n } = useTranslation()
+
+  return (
+    <>
+      <title>{t('title')}</title>
+      <meta name="description" content={t('description')} />
+      <meta name="keywords" content={t('keywords')} />
+      <meta name="robots" content="index, follow" />
+
+      {/* Open Graph Meta Tags */}
+      <meta property="og:title" content={t('openGraphTitle')} />
+      <meta property="og:description" content={t('openGraphDescription')} />
+      <meta property="og:image" content="https://trinko-next.vercel.app/logo/icon.png" />
+      <meta property="og:url" content={t('openGraphUrl')} />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="TrinkoTattoo" />
+      <meta property="og:locale" content={i18n.language} />
+
+      {/* Twitter Meta Tags */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="trinko-next.vercel.app" />
+      <meta property="twitter:url" content="https://trinko-next.vercel.app/" />
+      <meta name="twitter:title" content={t('openGraphTitle')} />
+      <meta name="twitter:description" content={t('openGraphDescription')} />
+      <meta name="twitter:image" content="https://trinko-next.vercel.app/logo/icon.png" />
+    </>
+  )
+}
